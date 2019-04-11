327121d6113a5786623b07b83bc1d8b3a82ed04e
visualstudio <visualstudio@230-PC09>
2019-04-11 13:42:42 -0400

    hh

--- c0703625/Program.cs
+++ c0703625/Program.cs
@@ -8,8 +8,38 @@ namespace c0703625
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+        }
+        public void Run() { this.ReadTextFiles(); }
+
+        public void ReadTextFiles()
+        {
+            {
+                int counter = 0;
+                string ln;
+                while ((in= FileStyleUriParser.ReadLine())!= null)
+                {
+                    Console.WriteLine(Ln);
+                    Beowulf.Add(Ln);
+                }
+                File.Close();
+                Console.WriteLine($"")
+            }
+        }
+        public int FindNumber of BlankSpaces( string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            foreach ( char c in line)
+            {
+                if (char.IsLetter(C)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+            }
+            return countSpaces;
         }
     }
 }
